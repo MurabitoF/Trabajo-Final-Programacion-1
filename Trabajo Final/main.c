dbb52daa3a88e735a524876e5bfd4838e8c4a182
EserskyD <eserskyd@users.noreply.github.com>
2020-06-05 17:02:24 -0300

    Armado de estructuras

--- Trabajo Final/main.c
+++ Trabajo Final/main.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include "gotoxy.h"
+typedef struct
+{
+    char nombre[30];
+    char apellido [30];
+    char domicilio [40];
+    char telefono;
+    char email[40];
+    int idCliente;
+    int bajaCliente;
+    int contrasena;
+} stCliente
+
+typedef struct
+{
+    char descPedido [240];
+    float costoPedido;
+    char fechaPedido[20];
+    int idCliente;
+    int idPedido;
+    int pedidoAnulado;
+} stPedido
 
 int main()
 {
